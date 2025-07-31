762e7daf012afea3913ba2dd90a82fd5ad3fad59
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-31 16:51:32 +0600

    Counting node in binary tree

---
+++ countNodeInBinaryTree.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int integerValue;
+        Node *leftNode;
+        Node *rightNode;
+
+        Node(int integerValue){
+            this->integerValue = integerValue;
+            this->leftNode = NULL;
+            this->rightNode = NULL;
+        }
+};
+
+Node *inputTree()
+{
+    
+}
+
+int main()
+{
+
+
+    return 0;
+}
